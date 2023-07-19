b81dcef27acfd691a8bfe6397250023b8f9c2501
meohoipeo249 <111524703+meohoipeo249@users.noreply.github.com>
2023-07-20 00:15:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '18 Tủi';
+const giftUrl = 'https://www.facebook.com/crystal.l.pham';
+const eventName = 'Happy Birthday Chị Gái ';
+const titleCard = 'Tặng Chị Gái';
+const contentCard = 'Chúc Bà Chị 18 tủi vẫn mãi xênh bớt giận hờn và yêu thằng em này nhiều hơn nha';
+
+
+const giftImage = 'Lonapham.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
